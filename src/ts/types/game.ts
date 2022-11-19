42e6c6835bca849ae2a691d4cfbfe6d1832d01da
Eviive <albert.vaillon21@gmail.com>
2022-11-19 14:55:57 +0100

    Added the base logic for the game - creation of the level based on the file - movement (direction) - drawing and iterating every X seconds

--- src/ts/types/game.ts
+++ src/ts/types/game.ts
@@ -7,3 +7,20 @@ export interface LevelFile {
 	food: Coordinates[];
 	snake: Coordinates[];
 };
+
+export type GameMap = number[][];
+
+export enum Tile {
+	Empty = 0,
+	Wall = 1,
+	Food = 2,
+	SnakeBody = 3,
+	SnakeHead = 4
+};
+
+export enum Direction {
+	Up = 1,
+	Right = 2,
+	Down = -1,
+	Left = -2
+};
